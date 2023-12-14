af169831c14e80a1666d029ffa1a0d9015b42eef
Timo Steinkamp <timosteinkamp@gmail.com>
2023-12-14 13:02:49 +0100

    Home: Add title and description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script lang="ts"></script>
+
+<svelte:head>
+	<title>Canapes · Reliable web components for everyday use.</title>
+	<meta name="description" content="Reliable web components for everyday use.">
+</svelte:head>
+
 <h1 class="logo">Canapés</h1>
 <p>Reliable web components for everyday use.</p>
