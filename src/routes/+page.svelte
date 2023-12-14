dc8a9ac0eddd802c4a9a564c51e35c453fe45272
Timo Steinkamp <timosteinkamp@gmail.com>
2023-12-14 12:27:55 +0100

    Home: Edit content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Canapés</h1>
+<p>Reliable web components for everyday use.</p>
