744ab50b6014d489462495b7d34e142b8c968c39
Timo Steinkamp <timosteinkamp@gmail.com>
2023-12-14 13:00:51 +0100

    Canonical: Add canonical tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
   import Navigation from '$lib/components/Navigation.svelte'
+  import { page } from '$app/stores'
 
   import '$lib/styles/app.css'
   import '$lib/styles/fonts.css'
 </script>
+
+<svelte:head>
+  <link rel="canonical" href="https://getcanapes.com{ $page.url.pathname != '/' ? $page.url.pathname + $page.url.search : '' }">
+</svelte:head>
 
 <Navigation />
 <main>
