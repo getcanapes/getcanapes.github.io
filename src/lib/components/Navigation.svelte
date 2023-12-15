8d82acbee3bd1c2ab7f41ab0d258263e14603041
Timo Steinkamp <timosteinkamp@gmail.com>
2023-12-15 10:23:52 +0100

    Navigation: Add pages

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -16,6 +16,31 @@
         <li class="navigation__item">Usage</li>
       </ul>
     </div>
+    <div class="navigation__section">
+      <h3 class="navigation__subline">Customization</h3>
+      <ul>
+        <li class="navigation__item">Design Tokens</li>
+        <li class="navigation__item">Color schemes</li>
+        <li class="navigation__item">Component styles</li>
+      </ul>
+    </div>
+    <div class="navigation__section">
+      <h3 class="navigation__subline">Components</h3>
+      <ul>
+        <li class="navigation__item">Badge</li>
+        <li class="navigation__item">Breadcrumb</li>
+        <li class="navigation__item">Breadcrumb item</li>
+        <li class="navigation__item">Button</li>
+        <li class="navigation__item">Details</li>
+        <li class="navigation__item">Icon</li>
+        <li class="navigation__item">Progress bar</li>
+        <li class="navigation__item">QR code</li>
+        <li class="navigation__item">Relative time</li>
+        <li class="navigation__item">Shortened text</li>
+        <li class="navigation__item">Skeleton</li>
+        <li class="navigation__item">Stage</li>
+      </ul>
+    </div>
   </div>
 </nav>
 
