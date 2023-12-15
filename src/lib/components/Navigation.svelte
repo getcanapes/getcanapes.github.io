a54663888030cc4d214b4e51415a5b03d05b34e5
Timo Steinkamp <timosteinkamp@gmail.com>
2023-12-15 12:05:13 +0100

    Navigation: Edit labels

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -8,7 +8,7 @@
   </div>
   <div class="navigation__body">
     <div class="navigation__section">
-      <h3 class="navigation__subline">Getting started</h3>
+      <h3 class="navigation__subline">Getting Started</h3>
       <ul class="navigation__items">
         <li class="navigation__item">Home</li>
         <li class="navigation__item">Installation</li>
@@ -19,8 +19,8 @@
       <h3 class="navigation__subline">Customization</h3>
       <ul class="navigation__items">
         <li class="navigation__item">Design Tokens</li>
-        <li class="navigation__item">Color schemes</li>
-        <li class="navigation__item">Component styles</li>
+        <li class="navigation__item">Color Schemes</li>
+        <li class="navigation__item">Component Styles</li>
       </ul>
     </div>
     <div class="navigation__section">
@@ -28,14 +28,14 @@
       <ul class="navigation__items">
         <li class="navigation__item">Badge</li>
         <li class="navigation__item">Breadcrumb</li>
-        <li class="navigation__item">Breadcrumb item</li>
+        <li class="navigation__item">Breadcrumb Item</li>
         <li class="navigation__item">Button</li>
         <li class="navigation__item">Details</li>
         <li class="navigation__item">Icon</li>
-        <li class="navigation__item">Progress bar</li>
-        <li class="navigation__item">QR code</li>
-        <li class="navigation__item">Relative time</li>
-        <li class="navigation__item">Shortened text</li>
+        <li class="navigation__item">Progress Bar</li>
+        <li class="navigation__item">QR Code</li>
+        <li class="navigation__item">Relative Time</li>
+        <li class="navigation__item">Shortened Text</li>
         <li class="navigation__item">Skeleton</li>
         <li class="navigation__item">Stage</li>
       </ul>
