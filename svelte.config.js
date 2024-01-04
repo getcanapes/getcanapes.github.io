71a00476497ba6698a5cdfa69487d30f49619df8
Timo Steinkamp <timosteinkamp@gmail.com>
2024-01-04 12:40:39 +0100

    Config: Remove unused code

--- svelte.config.js
+++ svelte.config.js
@@ -18,9 +18,6 @@ const config = {
       precompress: false,
       strict: true
     }),
-    /*paths: {
-      base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-    },*/
     prerender: {
       handleHttpError: ({path, _referrer, message}) => {
         if (path === '/404') {
