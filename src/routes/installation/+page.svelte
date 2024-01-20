7c7e95470b91c69f912814919bf9995f89f32b37
Timo Steinkamp <timosteinkamp@gmail.com>
2024-01-20 16:13:19 +0100

    Installation: Edit content

--- src/routes/installation/+page.svelte
+++ src/routes/installation/+page.svelte
@@ -34,7 +34,7 @@
   <code>
     &lt;script type="module" src="https://unpkg.com/@canapes/canapes@0.9.0/modules/components/badge/badge.js"&gt;&lt;/script&gt;
   </code>
-  <p>In a professional website, it is advisable not to reference the files via <a href="https://unpkg.com" target="_blank">unpkg.com</a>, but to store them on your own server. Simply copy the Canapés <i>modules</i> folder to your server and change the <i>src</i> in the import statement.</p>
+  <p>In a professional project, it is advisable not to reference the files via <a href="https://unpkg.com" target="_blank">unpkg.com</a>, but to store them on your own server. Simply copy the Canapés <i>modules</i> folder and change the <i>src</i> in your import statement.</p>
 </ca-stage>
 
 <ca-stage spacing-top="medium">
@@ -47,25 +47,10 @@
 
 <ca-stage spacing-top="small">
   <p>Simply install the latest version with the following command:</p>
-</ca-stage>
-
-<ca-stage spacing-top="small">
   <code>npm i @canapes/canapes</code>
-</ca-stage>
-
-<ca-stage spacing-top="small">
   <p>Or alternatively use yarn:</p>
-</ca-stage>
-
-<ca-stage spacing-top="small">
   <code>yarn i @canapes/canapes</code>
-</ca-stage>
-
-<ca-stage spacing-top="small">
   <p>After that you can import the component you want to use in your <i>script</i> part:</p>
-</ca-stage>
-
-<ca-stage spacing-top="small">
   <code>
     &lt;script&gt;<br>
     &nbsp;&nbsp;import '@canapes/canapes/lib/components/badge/badge.js'<br>
@@ -75,7 +60,8 @@
 
 <style>
   code {
+    font-size: var(--font-size-tiny);
     padding: var(--spacing);
-    margin: var(--spacing-large) 0;
+    margin: var(--spacing) 0 var(--spacing-large);
   }
 </style>
