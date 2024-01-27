7d65ee7b1fd89ed889a8913879d6e24844580c72
Timo Steinkamp <timosteinkamp@gmail.com>
2024-01-27 15:47:24 +0100

    ColorSchemes: Add page

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -43,7 +43,7 @@
           <a href="/customization/design-tokens" class="navigation__link" class:active={$page.url.pathname === '/customization/design-tokens'}>Design Tokens</a>
         </li>
         <li class="navigation__item">
-          <a href="/" class="navigation__link" class:active={$page.url.pathname === '/customization/color-schemes'}>Color Schemes</a>
+          <a href="/customization/color-schemes" class="navigation__link" class:active={$page.url.pathname === '/customization/color-schemes'}>Color Schemes</a>
         </li>
         <li class="navigation__item">
           <a href="/" class="navigation__link" class:active={$page.url.pathname === '/customization/custom-styles'}>Custom Styles</a>
---
+++ src/routes/customization/color-schemes/+page.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+  import '@canapes/canapes/lib/components/stage/stage.js'
+  
+  import Heading from '$lib/components/Heading.svelte'
+</script>
+
+<svelte:head>
+	<title>Color Schemes · Canapés</title>
+	<meta name="description" content="How to use color schemes in Canapés.">
+</svelte:head>
+
+<ca-stage>
+  <h1>Color Schemes</h1>
+</ca-stage>
+
+<ca-stage spacing-top="medium">
+  <p>In Canapés, color schemes can be defined using CSS variables. These color schemes are automatically taken into account by many components.</p>
+</ca-stage>
+
+<ca-stage spacing-top="medium">
+  <Heading>Defining Color Schemes</Heading>
+</ca-stage>
+
+<ca-stage spacing-top="small">
+  <p>Color schemes are defined as css variables. They need a <i>--ca</i> prefix, the <i>color-scheme</i> keyword, a name and a property. Here is an example of a color scheme called "success":</p>
+  <code>
+    &lt;style&gt;<br>
+    &nbsp;&nbsp;--ca-color-scheme-success-background-color: green;<br>
+    &nbsp;&nbsp;--ca-color-scheme-success-border-color: green;<br>
+    &nbsp;&nbsp;--ca-color-scheme-success-color: white;<br>
+    &lt;/style&gt;<br>
+  </code>
+</ca-stage>
+
+<ca-stage spacing-top="medium">
+  <Heading>Using Color Schemes</Heading>
+</ca-stage>
+
+<ca-stage spacing-top="small">
+  <p>Once a color scheme has been defined, it can be transferred as an attribute for many components:</p>
+  <code>
+    &lt;ca-button color-scheme="success"&gt;Foo&lt;/ca-button&gt;
+  </code>
+  <p>Detailed information on how the color scheme affects the component can be found on the respective documentation page of the component.</p>
+</ca-stage>
+
+<style>
+  code {
+    font-size: var(--font-size-tiny);
+    padding: var(--spacing);
+    margin: var(--spacing) 0 var(--spacing-large);
+  }
+</style>
